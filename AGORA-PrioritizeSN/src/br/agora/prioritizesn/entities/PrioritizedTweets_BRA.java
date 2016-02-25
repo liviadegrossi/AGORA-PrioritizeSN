8817b0a4aab2f2d688052228bef0fd52c940874d
luizassis <luizffga@dpi.inpe.br>
2016-02-25 16:11:11 -0300

    Java project

---
+++ AGORA-PrioritizeSN/src/br/agora/prioritizesn/entities/PrioritizedTweets_BRA.java
@@ -0,0 +1,260 @@
+package br.agora.prioritizesn.entities;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import br.agora.prioritizesn.utils.Common;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class PrioritizedTweets_BRA extends HttpServlet
+{
+	
+    /**
+	 * 
+	 */
+	public static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException 
+	{	
+		try {
+			//System.out.println("Prioritization of Twitter in Brazil");
+			receiveTweets();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error init - Twitter Brazil - "+e.getMessage());
+		}
+	}
+	
+	public static void receiveTweets() throws TwitterException
+    {
+		
+		// A builder that can be used to construct a twitter4j configuration with desired settings.
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+	    cb.setDebugEnabled(true);
+	    cb.setOAuthConsumerKey("njkpEt2VY49p3HhWmaVhIG7Wh");
+	    cb.setOAuthConsumerSecret("n853ektXcHy5OZZJ3PBLgstH1sqfvLG7waeQYGBvC3QM8E9v0r");
+	    cb.setOAuthAccessToken("1240286418-8iu0g5ZvfJkGLtIc8ZdbGDuIc7S36FtXXm8tvCT");
+	    cb.setOAuthAccessTokenSecret("tKdDKXLPIaquYpHuPSPNSzU7r6BdRJFqDnT29eLw8d2IV");
+        
+		TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+    
+		StatusListener listener = new StatusListener() {		
+			
+			@Override
+			public void onStatus(Status status) 
+			{		
+				
+            	if (status.getGeoLocation() != null)
+    			{	   
+            		
+            		/* ****************** PERFORMANCE TRACKING ****************** */
+    				//long middlSt =  System.nanoTime();
+    				            		
+            		try {  
+            		
+	            		/****************** VERIFY THE SMALLEST DISTANCE BETWEEN THIS TWEET AND A FLOODED AREA **********************/
+		    			
+		    			// connection to the database
+            			Connection conn = Common.dbConnection("jdbc:postgresql://localhost:5433/SaoPauloPrioritization", "postgres", "anta200");
+            			//Connection conn = Common.dbConnection("jdbc:postgresql://localhost:5432/SaoPauloPrioritization", "postgres", "anta200");
+		    			//Connection conn = Common.dbConnection("jdbc:postgresql://localhost:5432/SaoPauloPrioritization", "postgres", "agora");
+		    			
+		    			if (conn != null)
+		    			{	    				
+		    				
+		    				//System.out.println("Prioritized Tweets Brazil connection open!");
+		    				// removing special characters of the status text in order to store into the database
+			    			String statusText = status.getText().replaceAll("'", " ");
+			    						    			
+		    				// verifying if the tweet is already within the database
+			    			Statement sq_stmt0 = conn.createStatement();	
+							String sql_str0 = "SELECT * FROM tweets WHERE tweet_id = '"+status.getId()+"';";
+							ResultSet rs0 = sq_stmt0.executeQuery(sql_str0);
+							boolean tweet_isDatabase = rs0.next();
+							rs0.close();
+							sq_stmt0.close();
+			    			
+							if (!tweet_isDatabase)
+							{
+				    			// select all current flooded areas
+								Statement sq_stmt1 = conn.createStatement();	    			
+				    			String sql_str1 = "SELECT * FROM flooded_areas WHERE flooded_final_time is NULL";		    			
+				    			ResultSet rs1 = sq_stmt1.executeQuery(sql_str1);
+				    			
+				    			// define a big distance in order to compare and get the smallest distance between a tweet and a flooded area 
+				    			double smallest_distance = 2111111111;
+				    			double aux;
+				    			
+				    			// initialize gid
+				    			String gid = null, flooded_initial_time = null;
+				    			
+				    			// while there are flooded areas do this loop
+				    			while(rs1.next())
+				    			{		    			
+				    				
+				    				// calculate the distance between the tweet and one flooded area 
+				    				Statement sq_stmt2 = conn.createStatement();	    				
+				    				String sql_str2 = "SELECT ST_DISTANCE_Sphere(ST_SetSRID(ST_MakePoint("+status.getGeoLocation().getLongitude()+","+status.getGeoLocation().getLatitude()+"), 4326), ST_TRANSFORM(area, 4326)) as distance FROM catchments WHERE gid = '"+rs1.getString("catchments_gid")+"';";
+				    				ResultSet rs2 = sq_stmt2.executeQuery(sql_str2);
+				    				
+				    				if(rs2.next())
+				    				{		
+				    					// assign the distance to a variable
+					    				aux = Double.parseDouble(rs2.getString("distance"));
+					    							    				
+					    				// compare this distance with the smallest distance and if it is smaller than smallest_distance is aux
+					    				if (aux < smallest_distance)
+					    				{	
+					    					// it is also important to store the gid of the catchment associated with the smallest distance between a flooded area and the tweet
+					    					smallest_distance = aux;
+					    					gid = rs1.getString("catchments_gid");
+					    					flooded_initial_time = rs1.getString("flooded_initial_time");
+					    				}
+				    				}
+				    						    				
+				    				// close result set and connection
+				    				rs2.close();
+				    				sq_stmt2.close();
+				    				
+				    			}
+				    					    			
+				    			// close result set and connection
+				    			rs1.close();
+			    				sq_stmt1.close();
+			    				
+			    				//System.out.println("Twitter Brazil - "+status.getId());
+				    			/****************** INSERT THE TWEET AND ITS COMPONENTS INTO THE DATABASE **********************/
+				    			// if there is a flooded area, then insert every information of the tweet into the database
+				    			if (gid != null)
+				    			{
+				    				Statement sq_stmt3 = conn.createStatement();
+				    				String sql_str3  = "INSERT INTO tweets (tweet_id, tweet_time, username, tweet_text, location, catchments_gid, flooded_initial_time, distance) VALUES ("+status.getId()+", '"+status.getCreatedAt()+"', '"+status.getUser().getScreenName()+"', '"+statusText+"', ST_SetSRID(ST_MakePoint("+status.getGeoLocation().getLongitude()+","+status.getGeoLocation().getLatitude()+"), 4326), '"+gid+"', '"+flooded_initial_time+"', "+smallest_distance+");";
+									sq_stmt3.executeUpdate(sql_str3);
+									sq_stmt3.close();
+								}
+				    			// if there is no flooded area, then insert the tweet without catchments information into the database 
+				    			else
+				    			{		
+				    				Statement sq_stmt3 = conn.createStatement();
+				    				String sql_str3  = "INSERT INTO tweets (tweet_id, tweet_time, username, tweet_text, location) VALUES ("+status.getId()+", '"+status.getCreatedAt()+"', '"+status.getUser().getScreenName()+"', '"+statusText+"', ST_SetSRID(ST_MakePoint("+status.getGeoLocation().getLongitude()+","+status.getGeoLocation().getLatitude()+"), 4326));";
+									sq_stmt3.executeUpdate(sql_str3);
+									sq_stmt3.close();
+								}
+							
+							}
+									    				
+			    			conn.close();
+			    			//System.out.println("Prioritized Tweets Brazil connection closed!");
+		    			}
+		    			else
+		    				System.out.println("Prioritized Tweets Brazil - connection refused!!");
+		    					    				    				
+            		} catch (Exception e) {
+		    			System.out.println("Error Prioritized Tweets Brazil - "+e);
+		            }    
+            		
+            		/* ****************** PERFORMANCE TRACKING ****************** */
+    	            /*long middlEn = System.nanoTime();
+    	            
+    	            double x = Math.pow(10, -18);
+    				double a = ((Long.parseLong(String.valueOf(middlEn)) - Long.parseLong(String.valueOf(middlSt))) / x);
+    				
+    				a = a * Math.pow(10, -(Math.floor(Math.log10(a) - 18))); 
+    				
+    				SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"); 
+    				String date = dt.format(Calendar.getInstance().getTime());           
+    				
+    				String line = "PrioritizationTweetsBRA;" + date + ";" + middlSt + ";" + middlEn + ";" + (middlEn-middlSt) +";"+
+    						 a + ";" + status.getId() + ";";       
+    				
+    				Common.updateTwitterPerformanceMeasurement(line);*/
+            		
+    			}           	
+	            	
+	        }
+	    				    			
+			// called upon deletionNotice notices.
+	        @Override
+	        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+	            System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+	        }
+	
+	        // this notice will be sent each time a limited stream becomes unlimited. 
+	        // If this number is high and or rapidly increasing, it is an indication that your predicate is too broad, and you should consider a predicate with higher selectivity.
+	        @Override
+	        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+	            System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+	        }
+	
+	        // called upon location deletion messages.
+	        @Override
+	        public void onScrubGeo(long userId, long upToStatusId) {
+	            System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+	        }
+	        
+	        @Override
+	        public void onException(Exception ex) {
+	        	System.out.println("API Twitter Brazil error = "+ex);
+	        }
+	
+	        // called when receiving stall warnings.
+	     	@Override
+			public void onStallWarning(StallWarning arg0) {
+				// TODO Auto-generated method stub		
+			}
+	    };   
+	    
+	    // calculating bounding box
+    	List<double[]> rowList = new ArrayList<double[]>();
+    	
+    	double xmin = -53.183, ymin = -25.127, xmax = -44.0938,	ymax = -19.6895;
+
+		double xmin_i, ymin_i, xmax_i, ymax_i;
+		
+		//double loc[][] = new double [50][2];
+		
+		for (int i=0; i<5;i++)
+			for (int j=0; j<5; j++)
+			{
+				xmin_i = xmin + (i*(xmax-xmin)/5);
+				ymin_i = ymin + (j*(ymax-ymin)/5);
+				xmax_i = xmin + ((i+1)*(xmax-xmin)/5);
+				ymax_i = ymin + ((j+1)*(ymax-ymin)/5);
+				rowList.add(new double[] {xmin_i, ymin_i});
+				rowList.add(new double[] {xmax_i, ymax_i});
+			}    
+	    
+	    double loc[][] = new double [50][2];
+	    
+	    for (int i = 0; i < 50; i++) {
+	        loc[i][0] = rowList.get(i)[0];
+	        loc[i][1] = rowList.get(i)[1];
+	    }
+	    
+	    // creates a new FilterQuery	    
+	    FilterQuery fq = new FilterQuery();
+      
+        twitterStream.addListener(listener);
+           
+        fq.locations(loc);
+
+        twitterStream.filter(fq);
+	    
+    }
+    
+}
---
+++ AGORA-PrioritizeSN/src/br/agora/prioritizesn/utils/Common.java
@@ -0,0 +1,389 @@
+package br.agora.prioritizesn.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.imageio.ImageIO;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HTTP;
+import org.json.simple.JSONValue;
+import org.w3c.dom.Document;
+
+/**
+ * 
+ * @author sam
+ *
+ */
+public class Common {
+
+	/**
+	 * 
+	 */
+	public final static String SERVER_PATH = "/usr/share/tomcat7/webapps/52nSOSv3/WEB-INF/conf/sensors/";
+//	public final static String SERVER_PATH = "E:\\Desenvolvimento\\Agora\\AgoraDSM\\docs\\";
+	//public final static String SERVER_PATH = "E:\\Desenvolvimento\\Agora\\AgoraDSM\\docs\\";
+	
+//	public final static String SOS_PATH = "http://www.agora.icmc.usp.br:8080/52nSOSv3/sos";
+  //public final static String SOS_PATH = "http://localhost:8080/52nSOSv3/sos";
+//	public final static String SOS_PATH = "http://localhost:8081/52nSOSv3/sos";
+	public final static String SOS_PATH = "http://localhost:8080/52nSOSv3/sos";
+	
+	public final static int SOS_52n_VERSION = 3;
+	
+	public final static String IMAGES_PATH = "/home/suporte/uavs/";
+	//public final static String IMAGES_PATH = "/home/ftpuser/uavs/";
+//	public final static String IMAGES_PATH = "E:\\Desenvolvimento\\Agora\\AgoraDSM\\docs\\";
+	
+	public final static String uavsTrackingFile = "/home/suporte/AGORADSM/performance-tracking.csv";
+//	public final static String uavsTrackingFile = "E:\\Desenvolvimento\\Agora\\AgoraDSM\\docs\\performance-tracking.csv";
+	//public final static String uavsTrackingFile = "C:\\performance-tracking.csv";
+	
+	public final static String twitterTrackingFile = "/home/suporte/AGORADSM/twitter-performance-tracking.csv";
+	//public final static String twitterTrackingFile = "/home/ftpagora/AgoraDSM/twitter-performance-tracking.csv";
+	//public final static String twitterTrackingFile = "E:\\Desenvolvimento\\Agora\\AgoraDSM\\docs\\twit-performance-tracking.csv";
+	//public final static String twitterTrackingFile = "C:\\Users\\temp\\twitter-performance-tracking.csv";
+	
+	/**
+	 * 
+	 * @param line
+	 */
+	public static void updateUavsPerformanceMeasurement (String line) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(uavsTrackingFile, true));
+			
+			writer.write(line);
+			writer.newLine();
+			writer.flush();			
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param line
+	 */
+	public static void updateTwitterPerformanceMeasurement (String line) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(twitterTrackingFile, true));
+			
+			writer.write(line);
+			writer.newLine();
+			writer.flush();			
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static Document readXML(String url) {
+
+		Document doc = null;
+
+		try {
+
+			// reading XML file
+			File xmlFile = new File(url);
+
+			// get the DOM Builder Factory
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+
+			// ignoring comments
+			// dbFactory.setIgnoringComments(true);
+
+			// get the DOM Builder
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+			// load and parse the XML document
+			doc = dBuilder.parse(xmlFile);
+
+			// normalization of the XML document
+			// doc.getDocumentElement().normalize();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return doc;
+
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @throws TransformerException
+	 */
+	public static void printXML(Document doc) throws TransformerException {
+
+		// A TransformerFactory instance can be used to create Transformer and Templates objects. 
+		TransformerFactory tf = TransformerFactory.newInstance();
+		
+		// An instance of transformer can be obtained with the TransformerFactory.newTransformer method. This instance may then be used to process XML from a variety of sources and write the transformation output to a variety of sinks.
+		Transformer transformer = tf.newTransformer();
+		
+		// Set an output property that will be in effect for the transformation.
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		
+		// A character stream that collects its output in a string buffer, which can then be used to construct a string.
+		StringWriter writer = new StringWriter();
+		
+		// Transform the XML Source to a Result. Specific transformation behavior is determined by the settings of the TransformerFactory in effect when the Transformer was instantiated and any modifications made to the Transformer instance.
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		
+		// Return the string buffer itself.
+		String output = writer.getBuffer().toString().replaceAll("\n|\r", "");
+		
+		// print output
+		System.out.println(output);
+
+	}
+
+	/**
+	 * 
+	 * @param input
+	 * @throws HttpException
+	 * @throws IOException
+	 */
+	public static String httpPost(String xml) throws HttpException, IOException {
+		// prepare HTTP post
+		PostMethod post = new PostMethod(Common.SOS_PATH);
+
+		// request content will be retrieved directly from the input stream. Per
+		// default, the request content needs to be buffered in order to
+		// determine its length. Request body buffering can be avoided when
+		// content length is explicitly specified
+		// post.setRequestEntity(new InputStreamRequestEntity(new FileInputStream(input), input.length()));
+		post.setRequestEntity(new StringRequestEntity(xml, "application/xml", "UTF-8"));
+
+		// specify content type and encoding. If content encoding is not
+		// explicitly specified, ISO-8859-1 is assumed
+		post.setRequestHeader("Content-type", "application/xml; charset=UTF-8");
+
+		// get HTTP client
+		HttpClient httpclient = new HttpClient();
+
+		// execute request
+		try {
+
+			int result = httpclient.executeMethod(post);
+
+			// display status code
+			System.out.println("Response status code: " + result);
+
+			// display response
+			System.out.println("Response body: ");
+			System.out.println(post.getResponseBodyAsString());
+			
+			return post.getResponseBodyAsString();
+
+		} finally {
+			// release current connection to the connection pool once you are
+			// done
+			post.releaseConnection();
+		}
+
+	}
+	
+	/**
+	 * 
+	 * @param doc
+	 * @return
+	 * @throws TransformerException
+	 */
+	public static String getStringFromDocument(Document doc) throws TransformerException {
+		
+		// Acts as a holder for a transformation Source tree in the form of a Document Object Model (DOM) tree.
+		DOMSource domSource = new DOMSource(doc);
+	    
+		// A character stream that collects its output in a string buffer, which can then be used to construct a string.
+		StringWriter writer = new StringWriter();
+	    
+		// Acts as an holder for a transformation result, which may be XML, plain Text, HTML, or some other form of markup.
+		StreamResult result = new StreamResult(writer);
+	    
+		// A TransformerFactory instance can be used to create Transformer and Templates objects.
+		TransformerFactory tf = TransformerFactory.newInstance();
+	    
+		// An instance of transformer can be obtained with the TransformerFactory.newTransformer method. This instance may then be used to process XML from a variety of sources and write the transformation output to a variety of sinks.
+		Transformer transformer = tf.newTransformer();
+		
+		// Transform the XML Source to a Result. Specific transformation behavior is determined by the settings of the TransformerFactory in effect when the Transformer was instantiated and any modifications made to the Transformer instance.
+		transformer.transform(domSource, result);			
+		
+		return writer.toString();
+	}
+
+	/**
+	 * 
+	 * @param file
+	 * @return
+	 */
+	public static String ImageToBin(String file) {
+		byte[] imageInByte;
+		String binaryChain = "";
+
+		try {
+
+			BufferedImage originalImage = ImageIO.read(new File(file));
+
+			// convert BufferedImage to byte array
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(originalImage, "png", baos);
+			baos.flush();
+			imageInByte = baos.toByteArray();
+
+			binaryChain = imageInByte.toString();
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return binaryChain;
+
+	}
+
+	/**
+	 * 
+	 * @param xml
+	 * @param sosUrl
+	 * @return
+	 * @throws ClientProtocolException 
+	 * @throws IOException 
+	 */
+	public static String request(StringBuilder xml, String sosUrl) throws IOException {
+		
+		String response = new String();
+
+		String yourString = xml.toString();
+		
+		String result = yourString.replaceAll("^([\\W]+)<", "<");
+
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		
+		try {
+			
+			HttpPost httpPost = new HttpPost(sosUrl);
+			httpPost.addHeader("Content-Type", "text/xml");
+
+			StringEntity entity = new StringEntity(result, HTTP.UTF_8);
+			entity.setContentType("text/xml");
+			entity.setChunked(true);
+
+			httpPost.setEntity(entity);
+		
+			CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+
+			try {
+						
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						httpResponse.getEntity().getContent(), "ISO-8859-1"));
+				StringBuffer newData = new StringBuffer();
+				String data = new String();
+
+				while ((data = br.readLine()) != null) {
+					newData.append(data);
+				}
+
+				response = newData.toString();
+			
+			} finally {
+				httpResponse.close();
+			}	
+						
+		} finally {
+			httpClient.close();
+		}
+	
+		return response;
+		
+	}
+	
+	public static Connection dbConnection(String url, String user, String password) throws ClassNotFoundException, SQLException
+	{
+		
+		Class.forName("org.postgresql.Driver");
+				
+		return DriverManager.getConnection(url, user, password);
+		
+	}
+	
+	public static Object URLjsonToObject(String urlPage)
+	{
+		
+		String jsonString = null;
+		
+		try
+		{
+			// Class URL represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web		
+			URL jsonPage = new URL(urlPage);
+			HttpURLConnection connection = (HttpURLConnection) jsonPage.openConnection();
+						
+			//System.out.println("response code = "+connection.getResponseCode());
+			// not found - 400
+			// response ok - 200
+			if (connection.getResponseCode() == 200)
+			{				
+				// Reads text from a character-input stream, buffering characters so as to provide for the efficient reading of characters, arrays, and lines.
+				BufferedReader in = new BufferedReader(new InputStreamReader(jsonPage.openStream()));
+				
+		        // Get the contents of a byte[] as a String using the default character encoding of the platform.
+		        jsonString = IOUtils.toString(in);
+		        
+		        return JSONValue.parse(jsonString);
+		    }			
+	        
+			return null;
+			
+		}  catch(java.net.SocketException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error SocketException URLjsonToObject - Common - "+e);
+			return null;						
+		}	catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error URLjsonToObject - Common - "+e);
+			return null;
+		}		
+				
+	}
+
+}
